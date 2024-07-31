afb4eb0d1c8cd55185876fa236ae9b80a52b1d93
rupam office <rupamdhara.1579@gmail.com>
2024-07-31 18:47:36 +0530

    Updates

--- core/templatetags/core_tags.py
+++ core/templatetags/core_tags.py
@@ -1,10 +1,18 @@
 from django import template
-register = template.library()
-
+register = template.Library()
 from expense.models import *
+from django.contrib.auth import get_user_model
+from django.db.models.aggregates import Sum, Avg
+
+User = get_user_model()
 
 @register.filter(name='get_percentage')
 def get_used_budget_percentage(id, user_id):
     budget = Budget.objects.get(id=id)
-    if budget.duration == 'D':
-        spendings = Spending.objects.filter(user_id=user_id, budget=budget)
+    user = User.objects.get(id=user_id)
+
+    if budget.duration == 'M':
+        spendings = Spending.objects.filter(user=user, budget=budget)
+        total_spent = spendings.aggregate(Sum('amount'))['amount__sum'] if spendings else 0
+        percent = (total_spent * 100) / budget.amount
+    return percent
--- expense/models.py
+++ expense/models.py
@@ -28,7 +28,8 @@ class Budget(models.Model):
         
 
 class Spending(models.Model):
-    budget = models.ForeignKey(Budget, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='spending_user')
+    budget = models.ForeignKey(Budget, on_delete=models.CASCADE, related_name='spending_budget')
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     note = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
